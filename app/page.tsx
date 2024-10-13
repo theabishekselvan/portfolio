ecd3bf31cc98436b244a38cdb269200ef50d87e3
abishek <115536896+theabishekselvan@users.noreply.github.com>
2024-10-13 19:45:55 +0530

    added basic details

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,25 @@ import Intro from '@/components/intro'
 import NewsletterForm from '@/components/newsletter-form'
 import RecentPosts from '@/components/recent-posts'
 import RecentProjects from '@/components/recent-projects'
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <section className='pb-24 pt-40'>
       <div className='container max-w-3xl'>
+        <Head>
+          <title>Abishek Selvan</title>
+          <meta name="home page" content="This is the home page of your site" />
+        </Head>
+
         <Intro />
 
         <RecentPosts />
-        
+
         <RecentProjects />
 
         <NewsletterForm />
+
       </div>
     </section>
   )
